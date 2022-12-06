f85f416a7782c36ec51797a756040147978c6fe8
Asilbek1111 <99465065+Asilbek1111@users.noreply.github.com>
2022-12-07 01:17:49 +0300

    select

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -5,7 +5,6 @@ import Box from "@mui/material/Box";
 import { useContext, useEffect, useState } from "react";
 import { MyContext } from "../Contexts/Context";
 
-
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -34,83 +33,84 @@ function a11yProps(index) {
 }
 
 export default function BasicTabs() {
-  
   const [value, setValue] = useState(0);
   const { teacherId, setteacherId } = useContext(MyContext);
   // const [status, setStatus] = useContext("")
   // const [employmentType, setEmployentType] = useContext("");
   // const [employmentFieldId, setemploymentFieldId] = useContext("");
   // const [leader, setLeader] = useContext(null)
-  // const [interestIds, setInterestIds] = useContext("")
+  const [interestIds, setInterestIds] = useState("");
+  const [study, setStudy] = useState("");
   // const [imgSrc, setImgSrc] = useState(null);
   // const [image, setImage] = useState(null);
-//  const handleImageUpload = (e) => {
-//    const img = e.target.files;
-//    const url = URL.createObjectURL(img[0]);
-//    setImgSrc(url);
-//    setImage(img);
-//  };
+  //  const handleImageUpload = (e) => {
+  //    const img = e.target.files;
+  //    const url = URL.createObjectURL(img[0]);
+  //    setImgSrc(url);
+  //    setImage(img);
+  //  };
 
-//   let Submit = ()=>{
-//     var formdata = new FormData();
-//     formdata.append("hemisStudentId", teacherId);
-//     formdata.append("status", "SINGLE");
-//     formdata.append("employmentType", "WORKS_FORMAL");
-//     formdata.append(
-//       "employmentFieldId",
-//       "045a5bd0-ae87-4180-8b6f-199a4e779988"
-//     );
-//     formdata.append("leader", "true");
-//     formdata.append("interestIds", "94b1af74-8e4f-4328-8554-c78a8d678d0f");
-//     formdata.append(
-//       "passportImg",
-//       // fileInput.files[0],
-//       "/C:/Users/admin/Pictures/1.jpg"
-//     );
-//     formdata.append(
-//       "doc",
-//       // fileInput.files[0],
-//       "/C:/Users/admin/Pictures/3.png"
-//     );
-//     formdata.append("interestIds", "f781fd04-8692-462a-9ea7-9d26d8eaa1d6");
-//     formdata.append("tutorId", "78a5a035-92de-4e89-9d24-c9e3707c3a30");
+  //   let Submit = ()=>{
+  //     var formdata = new FormData();
+  //     formdata.append("hemisStudentId", teacherId);
+  //     formdata.append("status", "SINGLE");
+  //     formdata.append("employmentType", "WORKS_FORMAL");
+  //     formdata.append(
+  //       "employmentFieldId",
+  //       "045a5bd0-ae87-4180-8b6f-199a4e779988"
+  //     );
+  //     formdata.append("leader", "true");
+  //     formdata.append("interestIds", "94b1af74-8e4f-4328-8554-c78a8d678d0f");
+  //     formdata.append(
+  //       "passportImg",
+  //       // fileInput.files[0],
+  //       "/C:/Users/admin/Pictures/1.jpg"
+  //     );
+  //     formdata.append(
+  //       "doc",
+  //       // fileInput.files[0],
+  //       "/C:/Users/admin/Pictures/3.png"
+  //     );
+  //     formdata.append("interestIds", "f781fd04-8692-462a-9ea7-9d26d8eaa1d6");
+  //     formdata.append("tutorId", "78a5a035-92de-4e89-9d24-c9e3707c3a30");
 
-//     var requestOptions = {
-//       method: "POST",
-//       body: formdata,
-//       redirect: "follow",
-//     };
+  //     var requestOptions = {
+  //       method: "POST",
+  //       body: formdata,
+  //       redirect: "follow",
+  //     };
 
-//     fetch(
-//       "https://university-docs-production.up.railway.app/api/student",
-//       requestOptions
-//     )
-//       .then((response) => response.text())
-//       .then((result) => console.log(result))
-//       .catch((error) => console.log("error", error));
-//   }
+  //     fetch(
+  //       "https://university-docs-production.up.railway.app/api/student",
+  //       requestOptions
+  //     )
+  //       .then((response) => response.text())
+  //       .then((result) => console.log(result))
+  //       .catch((error) => console.log("error", error));
+  //   }
+  const [isLoaded5, setIsLoaded5] = useState(false);
+  const [ourData1, setOurData1] = useState([]);
 
-useEffect(() => {
-  fetch(
-    `https://university-docs-production.up.railway.app/api/student/info/${teacherId}`
-  )
-    .then((response) => response.json())
-    .then(
-      (res) => {
-        console.log(res.data.student);
-        setOurData(res.data.student);
-        setIsLoaded2(false);
-        // console.log(ourData)
-      },
+  useEffect(() => {
+    fetch(
+      `https://university-docs-production.up.railway.app/api/student/info/${teacherId}`
+    )
+      .then((response) => response.json())
+      .then(
+        (res) => {
+          console.log(res.data);
+          setOurData1(res.data);
+          setIsLoaded5(false);
+          // console.log(ourData)
+        },
 
-      (error) => {
-        setIsLoaded2(true);
-        setError2(error);
-      }
-    );
-}, []);
-  
-  
+        (error) => {
+          setIsLoaded5(true);
+          console.log(error);
+        }
+      );
+  }, []);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -206,28 +206,11 @@ useEffect(() => {
                     aria-label="Default select example"
                     id="exampleInputEmail2"
                     placeholder="Interest of student"
+                    onChange={(e) => setInterestIds(e.target.value)}
                   >
-                    <option value="03de6e03-c58b-4d91-a2ee-b3c1ee5dab37">
-                      Kitob o'qish
-                    </option>
-                    <option value="26158977-e790-4901-ba2b-6dad2e1be761">
-                      Rassomchilik
-                    </option>
-                    <option value="bd43ea89-2401-47a1-8a56-53e71a52d297">
-                      Pazandachilik
-                    </option>
-                    <option value="f781fd04-8692-462a-9ea7-9d26d8eaa1d6">
-                      IT sohasi
-                    </option>
-                    <option value="94b1af74-8e4f-4328-8554-c78a8d678d0f">
-                      Marketing
-                    </option>
-                    <option value="af3384f6-5684-40ec-a82b-fd4e1e463e3e">
-                      Sport
-                    </option>
-                    <option value="09404c40-00e7-45ee-813e-f297736b9b41">
-                      Musiqa
-                    </option>
+                    {ourData1.interests?.map((e) => (
+                      <option value={e.id}>{e.name}</option>
+                    ))}
                   </select>
                 </div>
               </div>
@@ -298,13 +281,23 @@ useEffect(() => {
                     <div className="form-group">
                       <label>Is Leader?</label>
 
-                      <h2 className="form-control">
-                        <select className="form-select">
-                          <option value="true">Yes</option>
-                          <option value="false">No</option>
-                          {/* <option value="divorced">Divorced</option> */}
-                        </select>
-                      </h2>
+                      <select className="form-select">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                        {/* <option value="divorced">Divorced</option> */}
+                      </select>
+                      <label>Studies</label>
+                      <select
+                        className="form-select"
+                        // onChange={(e) => {
+                        //   setStudy(e.target.value);
+                        //   console.log(e.target.value);
+                        // }}
+                      >
+                        {ourData1.fields.STUDIES?.map((e) => (
+                          <option value={e.id}>{e.name}</option>
+                        ))}
+                      </select>
                     </div>
                   </form>
                 </div>
